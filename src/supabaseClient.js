a1cdb4131dea28acacbd9f433b5eac609cc1d065
GuruSaravanan2004 <gurusaravanan.m2004@gmail.com>
2026-04-30 21:32:20 +0530

    Added .env to gitignore to prevent secret leaks

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://fvgqumjtildmncgzhhwi.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2Z3F1bWp0aWxkbW5jZ3poaHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NDExMjQsImV4cCI6MjA5MDAxNzEyNH0.qjm1pQh3EETMJKp_l5SQzZqau5yINPWC1EX176dGuoQ"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
